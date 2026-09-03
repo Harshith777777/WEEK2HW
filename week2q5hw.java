9891f1a4fb0269efb27968b1cf6fd1f730ffbdbb
Harshith777777 <sb7904@srmist.edu.in>
2026-09-03 15:49:18 +0530

    Add files via upload

---
+++ week2q5hw.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class week2q5hw {
+
+    static void printFilteredWordFrequency(String feedback) {
+
+        feedback = feedback.toLowerCase();
+
+        feedback = feedback.replace(".", "");
+        feedback = feedback.replace(",", "");
+
+        String[] words = feedback.split("\\s+");
+
+        String[] stopWords = {"the", "was", "and", "a", "is", "of", "in"};
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+
+            boolean isStopWord = false;
+
+            for (String stop : stopWords) {
+                if (word.equals(stop)) {
+                    isStopWord = true;
+                    break;
+                }
+            }
+
+            if (!isStopWord) {
+                map.put(word, map.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        List<Map.Entry<String, Integer>> list =
+                new ArrayList<>(map.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String feedback = sc.nextLine();
+
+        printFilteredWordFrequency(feedback);
+    }
+}
